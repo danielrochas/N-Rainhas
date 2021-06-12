b43e387187c527a4ac97c9a88432e0ce8b9fcb3b
Drope <ladeiraloes@gmail.com>
2021-06-12 14:24:20 -0300

    Atualiza quatroRainhasSemTabuleiro.pl
    Alteração nos comentários do código

--- PROLOG/quatroRainhasSemTabuleiro.pl
+++ PROLOG/quatroRainhasSemTabuleiro.pl
@@ -1,82 +1,85 @@
 
-%------------------------------------------------------------------------------------    
+%------------------------------------------------------------------------------------------------------------    
 % Problema das 4 rainhas
 
-%	Data: 30/05/2021
+% Data: 12/06/2021
 
-% -----------------------------------------------------------------------------------
-
-% Declara regras da posição de rainhas, exemplo Linhas = [2, 4, 1, 3]
-rainhas(N, Posicao) :- lista_rainhas(N, Linhas), 
+%------------------------------------------------------------------------------------------------------------
+%
+% Declara procedimento para posicionamento de rainhas, exemplo Linhas = [2, 4, 1, 3]
+rainhas(N, Posicao) :- 
+    
+    % Verifica condição de procedimento lista_rainhas
+    lista_rainhas(N, Linhas), 
+    
+    % Verifica condição de chamada do procedimento rainhas de forma recursiva
 	rainhas(Linhas , [], Posicao).
 
 % Declara predicado de posicionamento das rainhas
 rainhas([], Posicao , Posicao). 
 
-% ------------------------------------------------------------------------------------
+%------------------------------------------------------------------------------------------------------------
 
-% Declara regra de posicionamento de rainhas
+% Declara procedimento de posicionamento das rainhas
 rainhas(NaoPosicionada , Posicionada , Posicao) :-
     
-    % Seleciona nova rainha exemplo R = 4, Nova não posicionada = [3,2,1]
+    % Verifica condição de seleção de NaoPosicionada na lista R retorna lista NovaNaoPosicionada
 	select(NaoPosicionada , R, NovaNaoPosicionada ), 
     
-    % Condição de falha no ataque
+    % Verifica condição de falha no ataque
 	posicaoSegura(Posicionada , R, 1), 
     
-    % Se a posição é segura escolhe a próxima rainha
+    % Verifica condição de chamada recursiva do procedimento rainhas para posicionar NovaNaoPosicionada
 	rainhas(NovaNaoPosicionada , [R|Posicionada], Posicao).
 
-% ------------------------------------------------------------------------------------
-
+%------------------------------------------------------------------------------------------------------------
 % Declara predicado de posição segura
 posicaoSegura([], _Rainha , _Nb).
 
-% Declara regra de posição segura de forma recursiva
+% Declara procedimento de posição segura de forma recursiva
 posicaoSegura([Y|Ys], Rainha , Nb) :-
     
-    % Comparação de igualdade com rainhas já posicionadas pra frente e pra trás
+    % Verifica condição de comparação de igualdade com rainhas já posicionadas pra frente e pra trás
 	Rainha =\= Y + Nb ,
     Rainha =\= Y - Nb ,
     
-    % Declara Nb1 como aumento da distância para recursividade
+    % Verifica Condição de incremento de Nb em Nb1 para chamada recursiva do procedimento
     Nb1 is Nb + 1,
     
-    % Executa posição segura de forma recursiva
+    % Verifica condição de chamda recursiva  do procedimento posiçãoSegura para próxima posição
 	posicaoSegura(Ys , Rainha , Nb1).
 
-% ------------------------------------------------------------------------------------
-
+%------------------------------------------------------------------------------------------------------------
+%
 % Declara predicado de seleção 
 select([X|Ys], X, Ys).
 
-% Declara regra de seleção
+% Declara procedimento de seleção da variável X no restante da primeira lista Ys retornando lista Zs
 select([Y|Ys], X, [Y|Zs]) :- select(Ys , X, Zs).
 
-% ------------------------------------------------------------------------------------
+%------------------------------------------------------------------------------------------------------------
 
-% Declara predicado lista de rainhas
+% Declara predicado lista de rainhas iniciado relação de lista_rainhas do inteiro 0 com lista vazia
 lista_rainhas(0, []).
 
-% Declara regra para lista de rainhas
+% Declara procedimento para verificar se a variável N é a primeira posição na lista
 lista_rainhas(N, [N|Linhas]) :-
     
-    % Condição de N maior que 0
+    % Verifica condição de N maior que 0
 	N > 0,
     
-    % Declara incremento de N em N1 para recursividade
+    % Verifica condição para atualizar decremento de contador N1 para chamada recursiva de lista_rainhas
     N1 is N - 1,
     
-    % Executa função de forma recursiva até N <= 0
+    % Verifica condição de chamada do procedimento lista_rainhas de forma recursiva até N <= 0
     lista_rainhas(N1 , Linhas).
 
-% ------------------------------------------------------------------------------------
+%------------------------------------------------------------------------------------------------------------
+%
+% Consulta para executar programa
+% ? rainhas(4, Posicao)
 
-% Query para rodar algoritimo
-% rainhas(4, Posicao)
-
-% ------------------------------------------------------------------------------------
-
+%------------------------------------------------------------------------------------------------------------
 
 
 
